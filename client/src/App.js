ae287471b5a4e71bde77f9705300868e93bd16f5
julianathayil <julianathayil@gmail.com>
2020-09-28 12:04:54 +0530

    Config Tailwind CSS :star:

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     
+    <div class="bg-green-900">
+      <h1 class="text-white text-lg"> Hello world</h1>
+      <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4">
+        Button
+      </button>
+
     </div>
   );
 }
